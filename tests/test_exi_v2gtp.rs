11b51504b29886236178a7912d589874cf82b312
Terence Stenvold <tstenvold@gmail.com>
2025-07-25 14:47:36 +0200

    Adding sample messages for future use

--- tests/test_exi_v2gtp.rs
+++ tests/test_exi_v2gtp.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use rustv2g::common::exi_error_codes::BITSTREAM_OVERFLOW;
+    use rustv2g::common::exi_error_codes::*;
     use rustv2g::exi_v2gtp::{
         v2gtp20_read_header, v2gtp20_write_header, v2gtp_read_header, v2gtp_write_header,
     };
@@ -9,7 +9,7 @@ mod tests {
         let mut buf = [0u8; 8];
         let payload_length = 0x12345678;
         let res = v2gtp_write_header(&mut buf, payload_length);
-        assert_eq!(res, Ok(NO_ERROR));
+        assert_eq!(res, Ok(NO_ERROR as usize));
         assert_eq!(buf, [0x1, 0xfe, 0x80, 0x01, 0x12, 0x34, 0x56, 0x78]);
     }
 
@@ -17,14 +17,14 @@ mod tests {
     fn test_v2gtp_write_header_buffer_too_small() {
         let mut buf = [0u8; 7];
         let res = v2gtp_write_header(&mut buf, 0x1234);
-        assert_eq!(res, Err(-3));
+        assert_eq!(res, Err(-3 as i16));
     }
 
     #[test]
     fn test_v2gtp20_write_header_custom_payload_id() {
         let mut buf = [0u8; 8];
         let res = v2gtp20_write_header(&mut buf, 0x01020304, 0x1234);
-        assert_eq!(res, Ok(NO_ERROR));
+        assert_eq!(res, Ok(NO_ERROR as usize));
         assert_eq!(buf, [0x1, 0xfe, 0x12, 0x34, 0x01, 0x02, 0x03, 0x04]);
     }
 
@@ -32,14 +32,14 @@ mod tests {
     fn test_v2gtp_read_header_success() {
         let buf = [0x1, 0xfe, 0x80, 0x01, 0x00, 0x00, 0x00, 0x10];
         let res = v2gtp_read_header(&buf);
-        assert_eq!(res, Ok(16));
+        assert_eq!(res, Ok(16 as usize));
     }
 
     #[test]
     fn test_v2gtp_read_header_too_short() {
         let buf = [0x1, 0xfe, 0x80, 0x01, 0x00, 0x00, 0x00];
         let res = v2gtp_read_header(&buf);
-        assert_eq!(res, Err(-3));
+        assert_eq!(res, Err(-3 as i16));
     }
 
     #[test]
@@ -53,13 +53,13 @@ mod tests {
     fn test_v2gtp20_read_header_wrong_payload_id() {
         let buf = [0x1, 0xfe, 0x12, 0x34, 0, 0, 0, 0];
         let res = v2gtp20_read_header(&buf, 0x8001);
-        assert_eq!(res, Err(-2));
+        assert_eq!(res, Err(-2 as i16));
     }
 
     #[test]
     fn test_v2gtp20_read_header_custom_payload_id_success() {
         let buf = [0x1, 0xfe, 0x12, 0x34, 0x00, 0x00, 0x00, 0x05];
         let res = v2gtp20_read_header(&buf, 0x1234);
-        assert_eq!(res, Ok(5));
+        assert_eq!(res, Ok(5 as usize));
     }
 }
